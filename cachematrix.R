966b52316a0c42991b5cdee8643d42b352941d4d
Mandapaka <KMANDAPA@volvocars.com>
2018-10-24 22:18:23 +0200

    Completed assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,75 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file has two functions: makeCacheMatrix and cacheSolve
+## makeCacheMatrix is a function that build up a funtion storing the inverse of 
+##                  a matrix in the lexical scope cache
 
-## Write a short comment describing this function
+## cacheSolve is a function that build up a funtion to evaluate the inverse if 
+##                  it is not cached in lexical scope, then store and return
+
+## Missing validation for input type as those are under assumption
+
+## Makecachematrix accepts a square matrix, defines
+## getter, setter for its value
+## getter and setter for cached inverse value
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## Initiate the inverse value to NULL 
+  xinv <- NULL
+  
+  ## Setter function for initializing the matrix and its inverse in lexical scope
+  setval <- function(y) {
+    x <<- y
+    xinv <<- NULL
+  }
+  
+  ## Getter to return the matrix
+  getval <- function() x
+  
+  ## Setter for storing inverse in lexical scope
+  setinv <- function(xinve) xinv <<- xinve
+  
+  ## Getter to return the matrix inverse from lexical scope
+  getinv <- function() xinv
+  
+  list(setval = setval, getval = getval,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve accepts a square matrix, retrieve chached inverse 
+## from lexical scope if it is available or evaluates the inverse,
+## stores and returns the same
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## Retrive cached inverse
+  xinv <- x$getinv()
+  
+  ## Validate if it is null or has valid value
+  if(!is.null(xinv)) {
+    message("getting cached data")
+    return(xinv)
+  }
+  
+  ## Evaluate the inverse
+  data <- x$getval()
+  ## print(data)
+  detval <- det(data)
+  
+  if(detval[1] == 0)
+  {
+    message("Determinant is 0. Inverse does not exist.")
+    return()
+  }
+  
+  xinv <- solve(data, ...)
+  
+  ## Store the inverse in cache
+  x$setinv(xinv)
+  
+  ## Return the inverse
+  xinv
 }
+
+
+
